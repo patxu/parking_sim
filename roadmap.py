7e16608192be1a542ef1e9bce8709bb6383f80c2
db2882 <david@dnbain.com>
2015-08-23 15:33:40 -0400

    radding changes

--- roadmap.py
+++ roadmap.py
@@ -20,8 +20,12 @@ class Coord:
       return False
   def increaseX(self,value):
     self.x += value
+  def decreaseX(self,value):
+    self.x -= value
   def increaseY(self,value):
     self.y += value
+  def decreaseY(self,value):
+    self.y -= value
   def __str__(self):
     return str(self.x)+','+str(self.y)
 
