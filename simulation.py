f46e9a22899a056a3857618ff8ca93c956f2a6af
db2882 <david@dnbain.com>
2015-08-23 16:10:24 -0400

    small changes:

--- simulation.py
+++ simulation.py
@@ -2,38 +2,47 @@ import simpy
 from roadmap import Coord,Direction,ParkingSpot,RoadSection,Road,RoadMap,loadCity
 
 class Car(object):
-	def __init__(self,env,wantsToPark,carID,cityMap,coordinates,currentStreetId):
+	def __init__(self,env,wantsToPark,carID,cityMap,coordinates,currentStreetId,direction):
 		self.env = env
 		self.action = env.process(self.run())
 		self.wantsToPark = wantsToPark
 		self.carID = carID
-		self.parkingSpots = parkingSpots
+		self.cityMap = cityMap
 		self.coordinates = coordinates
-		self.currentStreetId
+		self.currentStreetId = currentStreetId
+		self.direction = direction
 	
+	def move(self,direction):
+		if(self.direction == Direction.North):
+			self.coordinates.increaseY(1)
+		elif(self.direction == Direction.East):
+			self.coordinates.increaseX(1)
+		elif(self.direction == Direction.South):
+			self.coordinates.decreaseY(1)
+		elif(self.direction == Direction.West):
+			self.coordinates.decreaseX(1)
+		else:
+			print("invalid direction")
+
 	def run(self):
 		while True:
-			if (self.wantsToPark == True and self.parkingSpots[self.coordinates.x][self.coordinates.y] is not None):
-				if(self.parkingSpots[self.coordinates.x][self.coordinates.y].available==True):
-					print('Car %d Start parking at time %d at coord %d,%d' % (self.carID,self.env.now,self.coordinates.x,self.coordinates.y))
-					self.parkingSpots[self.coordinates.x][self.coordinates.y].request()
-					parking_duration = 5
-					yield env.timeout(parking_duration)
-					self.parkingSpots[self.coordinates.x][self.coordinates.y].release()
-					self.wantsToPark = False
-
+			
 			print ('Car %d driving at time %d at coord %d, %d' % (self.carID,self.env.now,self.coordinates.x,self.coordinates.y))
 			trip_duration = 1
-			self.coordinates.increaseY(1)
-			yield env.timeout(1)
+			self.move(self.direction)
+			yield self.env.timeout(trip_duration)
+			
 
 env = simpy.Environment()
 
 #create coordinate of car
-carCoord = Coord(1,1)
+carCoord = Coord(0,0)
+carCoord2 = Coord(2,0)
+roadMap = loadCity("cities/city2.xml")
+print roadMap.graph[1][1]
 
+car = Car(env,True,1,roadMap,carCoord,1,Direction.North)
 
-roadMap = loadCity("cities/city1.xml")
+car2 = Car(env,True,2,roadMap,carCoord2,2,Direction.North)
 
-while env.now < 9:
-	env.step()
+env.run(until=15)
